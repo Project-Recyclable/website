3f1d5ef795790ddd41443b37941c3d83088258ad
WillC919 <william.p.lee@stonybrook.edu>
2022-07-18 20:27:46 -0400

    init

--- script.js
+++ script.js
@@ -10,18 +10,29 @@ close.addEventListener('click', () => {
     nav.classList.remove('open-nav')
 })
 
-var dropdown = document.getElementsByClassName("dropbtn");
-var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    var dropdown = document.getElementsByClassName("dropbtn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
     }
-  });
+  } 
 }
+
+var x = window.matchMedia("(max-width: 920px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
 
